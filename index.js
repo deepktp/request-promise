8395946b67578d90b930648fabed8c031ccaf208
Deepak Yadav <76097094+deepktp@users.noreply.github.com>
2022-01-08 16:17:26 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const request = require('request');
+
+
+function rp(uri, options, callback) {
+        return new Promise(function (resolve, reject) {
+                request(uri, options, function (err, res, body) {
+                        if(callback){
+                                return callback(err, res, body)
+                        }else{
+                                if(err){
+                                        reject(err);
+                                }else{
+                                        resolve(body);
+                                }
+                        }
+                })
+        })
+}
+function verbFunc ( verb )
+{
+  var method = verb === 'del' ? 'DELETE' : verb.toUpperCase()
+  return function ( uri, options, callback ) {
+    var params = request.initParams( uri, options, callback )
+    params.method = method
+    return rp( params, params.callback )
+  }
+}
+
+
+rp.get = verbFunc( 'get' )
+rp.head = verbFunc( 'head' )
+rp.post = verbFunc( 'post' )
+rp.put = verbFunc( 'put' )
+rp.patch = verbFunc( 'patch' )
+rp.del = verbFunc( 'del' )
+
+rp.jar = request.jar
+rp.cookie = request.cookie
+rp.defaults = function () {
+  request = request.defaults.apply( lib, arguments )
+  request.initParams = lib.initParams
+  return rp
+}
